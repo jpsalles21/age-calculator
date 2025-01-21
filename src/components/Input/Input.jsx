14ffb9e39257c8a482bc8400d3f8e2599ec96d0d
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-01-21 16:10:17 -0300

    fix: enable backspace and delete

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,24 +1,33 @@
 import './styles.css';
 
-const Input = ({ label, place, maxLength }) => {
+const Input = ({ label, placeholder, maxLength, value, onChange }) => {
 
     const handleKeyPress = (e) => {
+        if (e.key === 'Backspace' || e.key === 'Delete') {
+            return; 
+        }
         if (!/[0-9]/.test(e.key)) {
             e.preventDefault();
         }
     };
+
+    const handleChange = (e) => {
+        onChange(e.target.value);
+    }
+
     return (
         <div className="input__container">
             <label>{label}</label>
             <input
                 type="text"
-                placeholder={place}
+                placeholder={placeholder}
                 maxLength={maxLength}
+                value={value}
                 onKeyDown={handleKeyPress}
-            >
-            </input>
+                onChange={handleChange}
+            />
         </div>
-    )
+    );
 }
 
-export default Input; 
+export default Input;

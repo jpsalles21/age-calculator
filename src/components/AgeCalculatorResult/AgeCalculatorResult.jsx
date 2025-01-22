10f174d62f5ed1d4cb102466ea68b023f887866b
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-01-22 12:11:01 -0300

    feat: add age calculator result

--- src/components/AgeCalculatorResult/AgeCalculatorResult.jsx
+++ src/components/AgeCalculatorResult/AgeCalculatorResult.jsx
@@ -1,11 +1,47 @@
+const AgeCalculatorResult = ({ year, month, day }) => {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = date.getMonth() + 1;
+    const currentDay = date.getDate();
+   
+    const calculateAge = () => {
+        let yearDiff = currentYear - year;
 
-const AgeCalculatorResult = ({year, month, day}) => {
+        if (currentMonth < month || (currentMonth === month && currentDay < day)) {
+            yearDiff -= 1;
+        }
+        return yearDiff;
+    };
 
-    const date = new Date()
-    
+    const calculateMonths = () => {
+        let monthDiff = currentMonth - month;
+
+        if (currentDay < day) {
+            monthDiff -= 1;
+        }
+        if (monthDiff < 0) {
+            monthDiff += 12;
+        }
+        return monthDiff;
+    };
+
+    const calculateDays = () => {
+        const lastBirthMonthDay = new Date(currentYear, month, 0).getDate();
+        let dayDiff = currentDay - day;
+
+        if (dayDiff < 0) {
+            dayDiff += lastBirthMonthDay;
+        }
+        return dayDiff;
+    };
+
     return (
-        <div>{`${year} ${month} ${day} ${date.getMonth()+1} `}</div>
-    )
-}
+        <div>
+            <p>Years: {calculateAge()}</p>
+            <p>Months: {calculateMonths()}</p>
+            <p>Days: {calculateDays()}</p>
+        </div>
+    );
+};
 
 export default AgeCalculatorResult;
